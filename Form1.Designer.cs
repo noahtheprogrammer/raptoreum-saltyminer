d4ce050d6972a9971ce487d2481d51d62462c46d
OKprogrammer <weavernoah11@gmail.com>
2021-07-16 14:45:33 -0400

    Initial commit
    Commited the first version of the raptoreum-saltyminer user interface.

---
+++ Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace raptoreum_rtminer
+{
+    partial class rtm_miner
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(rtm_miner));
+            this.mining_button = new System.Windows.Forms.Button();
+            this.address_text = new System.Windows.Forms.TextBox();
+            this.pool_text = new System.Windows.Forms.TextBox();
+            this.address_label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.set_box = new System.Windows.Forms.ListBox();
+            this.instruction_text = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.mine_text = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mining_button
+            // 
+            this.mining_button.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mining_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.mining_button.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(235)))), ((int)(((byte)(98)))));
+            this.mining_button.FlatAppearance.BorderSize = 5;
+            this.mining_button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(215)))), ((int)(((byte)(78)))));
+            this.mining_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mining_button.Image = global::raptoreum_rtminer.Properties.Resources.mine_start;
+            this.mining_button.Location = new System.Drawing.Point(585, 262);
+            this.mining_button.Name = "mining_button";
+            this.mining_button.Size = new System.Drawing.Size(125, 125);
+            this.mining_button.TabIndex = 1;
+            this.mining_button.UseVisualStyleBackColor = false;
+            this.mining_button.Click += new System.EventHandler(this.mining_button_Click);
+            // 
+            // address_text
+            // 
+            this.address_text.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
+            this.address_text.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.address_text.Font = new System.Drawing.Font("Neue Montreal Medium", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.address_text.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.address_text.Location = new System.Drawing.Point(12, 107);
+            this.address_text.Name = "address_text";
+            this.address_text.PlaceholderText = "  (ex. RWXMEVTEJYNVP2HTJQ97DMYVWYTWUFTI8E)";
+            this.address_text.Size = new System.Drawing.Size(305, 22);
+            this.address_text.TabIndex = 0;
+            this.address_text.TextChanged += new System.EventHandler(this.address_text_TextChanged);
+            // 
+            // pool_text
+            // 
+            this.pool_text.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
+            this.pool_text.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pool_text.Font = new System.Drawing.Font("Neue Montreal Medium", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.pool_text.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pool_text.Location = new System.Drawing.Point(12, 164);
+            this.pool_text.Name = "pool_text";
+            this.pool_text.PlaceholderText = "  (ex. stratum+tcp://r-pool.net:3008)";
+            this.pool_text.Size = new System.Drawing.Size(305, 22);
+            this.pool_text.TabIndex = 2;
+            this.pool_text.TextChanged += new System.EventHandler(this.pool_text_TextChanged);
+            // 
+            // address_label
+            // 
+            this.address_label.AutoSize = true;
+            this.address_label.Font = new System.Drawing.Font("Neue Montreal", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.address_label.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(77)))), ((int)(((byte)(77)))));
+            this.address_label.Location = new System.Drawing.Point(12, 84);
+            this.address_label.Name = "address_label";
+            this.address_label.Size = new System.Drawing.Size(169, 20);
+            this.address_label.TabIndex = 3;
+            this.address_label.Text = "Insert payout address:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Neue Montreal", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(77)))), ((int)(((byte)(77)))));
+            this.label1.Location = new System.Drawing.Point(12, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Insert mining pool:";
+            // 
+            // set_box
+            // 
+            this.set_box.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
+            this.set_box.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.set_box.Font = new System.Drawing.Font("Neue Montreal", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.set_box.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.set_box.FormattingEnabled = true;
+            this.set_box.ItemHeight = 15;
+            this.set_box.Items.AddRange(new object[] {
+            "AVX",
+            "AVX2",
+            "AVX512",
+            "AVX512-SHA",
+            "AVX512-SHA-VAES",
+            "SSE2",
+            "SSE42",
+            "SSSE3",
+            "ZEN",
+            "ZEN2",
+            "ZEN3"});
+            this.set_box.Location = new System.Drawing.Point(12, 220);
+            this.set_box.Name = "set_box";
+            this.set_box.Size = new System.Drawing.Size(227, 167);
+            this.set_box.TabIndex = 5;
+            this.set_box.SelectedIndexChanged += new System.EventHandler(this.set_box_SelectedIndexChanged);
+            // 
+            // instruction_text
+            // 
+            this.instruction_text.Location = new System.Drawing.Point(0, 0);
+            this.instruction_text.Name = "instruction_text";
+            this.instruction_text.Size = new System.Drawing.Size(100, 23);
+            this.instruction_text.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Neue Montreal", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(77)))), ((int)(((byte)(77)))));
+            this.label2.Location = new System.Drawing.Point(12, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Choose instruction set:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Neue Montreal Bold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(209, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(217, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "\"May the salt mine with you.\"";
+            // 
+            // mine_text
+            // 
+            this.mine_text.AutoSize = true;
+            this.mine_text.Font = new System.Drawing.Font("Neue Montreal", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.mine_text.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(77)))), ((int)(((byte)(77)))));
+            this.mine_text.Location = new System.Drawing.Point(573, 230);
+            this.mine_text.Name = "mine_text";
+            this.mine_text.Size = new System.Drawing.Size(137, 20);
+            this.mine_text.TabIndex = 9;
+            this.mine_text.Text = "Start CPU mining:";
+            // 
+            // rtm_miner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.ClientSize = new System.Drawing.Size(734, 411);
+            this.Controls.Add(this.mine_text);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.instruction_text);
+            this.Controls.Add(this.set_box);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.address_label);
+            this.Controls.Add(this.pool_text);
+            this.Controls.Add(this.address_text);
+            this.Controls.Add(this.mining_button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(750, 450);
+            this.Name = "rtm_miner";
+            this.Text = "SaltyMiner";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button mining_button;
+        private System.Windows.Forms.TextBox address_text;
+        private System.Windows.Forms.TextBox pool_text;
+        private System.Windows.Forms.Label address_label;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox set_box;
+        private System.Windows.Forms.Label instruction_text;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label mine_text;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace raptoreum_rtminer
+{
+
+    // Class that holds the rtm_miner form
+    public partial class rtm_miner : Form
+    {
+        // Process to be used for mining
+        Process process;
+
+        // Instruction set to be used for proper CPU
+        public string instruction_set;
+
+        // Bool to remember if CPU is mining
+        public bool _ismining = false;
+
+        // String used to hold RTM address
+        public string address;
+
+        // String used to hold pool of choice
+        public string pool;
+
+        // Initializes the rtm_miner component
+        public rtm_miner()
+        {
+            InitializeComponent();
+        }
+
+        // Starts or stops the mining whenever the button is clicked
+        private void mining_button_Click(object sender, EventArgs e)
+        {
+            if (_ismining == false)
+            {
+                RunMiner();
+                ButtonChange();
+                _ismining = true;
+            }
+
+            else if (_ismining == true)
+            {
+                QuitMiner();
+                ButtonChange();
+                _ismining = false;
+            }
+        }
+
+        // Turns on the CPU miner process
+        public void RunMiner()
+        {
+            ProcessStartInfo processInfo;
+            processInfo = new ProcessStartInfo(instruction_set +".exe", "-a gr -o " + pool + " -u " + address);
+            processInfo.CreateNoWindow = true;
+            process = Process.Start(processInfo);
+        }
+
+        // Turns off the CPU miner process
+        public void QuitMiner()
+        {
+            process.Kill();
+        }
+
+        // Gets the address text to mine
+        private void address_text_TextChanged(object sender, EventArgs e)
+        {
+            address = address_text.Text;
+        }
+
+        // Gets the pool text to mine
+        private void pool_text_TextChanged(object sender, EventArgs e)
+        {
+            pool = pool_text.Text;
+        }
+
+        // Gets the proper instruction set chosen to mine
+        private void set_box_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            instruction_set = set_box.Text; 
+        }
+
+        // Changes button color
+        private void ButtonChange()
+        {
+            if (_ismining == false)
+            {
+                mining_button.BackColor = Color.FromArgb(255, 192, 192);
+                mining_button.FlatAppearance.BorderColor = Color.FromArgb(255, 128, 128);
+                mining_button.FlatAppearance.MouseOverBackColor = Color.FromArgb(235, 108, 108);
+                mining_button.Image = Properties.Resources.mine_stop;
+                mine_text.Text = "Stop CPU mining:";
+            }
+
+            if (_ismining == true)
+            {
+                mining_button.BackColor = Color.FromArgb(128, 255, 128);
+                mining_button.FlatAppearance.BorderColor = Color.FromArgb(98, 235, 98);
+                mining_button.FlatAppearance.MouseOverBackColor = Color.FromArgb(78, 215, 78);
+                mining_button.Image = Properties.Resources.mine_start;
+                mine_text.Text = "Start CPU mining:";
+            }
+        }
+    }
+}
