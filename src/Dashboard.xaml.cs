cf0e58a91b80f88ae63a535ef0558cd11b5c0870
noahtheprogrammer <weavernoah11@gmail.com>
2021-11-23 22:18:30 -0500

    Exit and minimize buttons

--- src/Dashboard.xaml.cs
+++ src/Dashboard.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace Saltyminer
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Dashboard.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Dashboard : Window
     {
-        public MainWindow()
+        public Dashboard()
         {
             InitializeComponent();
         }
