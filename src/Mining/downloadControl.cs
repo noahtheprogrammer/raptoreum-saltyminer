a0dda780a1798c6043ddac8f0d13f37d30f2d668
noahtheprogrammer <81941019+noahtheprogrammer@users.noreply.github.com>
2022-01-24 13:11:14 -0500

    Removing unnecessary code

--- src/Mining/downloadControl.cs
+++ src/Mining/downloadControl.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net;
 using System.Threading.Tasks;
 using Octokit;
@@ -13,10 +13,6 @@ namespace Saltyminer.Mining
     {
         // WebClient used to download new releases
         WebClient d_client = new WebClient();
-
-        // Array used to hold info loaded on machine
-        // Below display what spot in the array each miner must specify
-        public string[] currentInstalls = { "0", "0", "0", "0", "0", "0", "0", "0", "0" };
 
         // Used to download latest release of miners
         public Task DownloadRelease(string link, string file, string folder)
@@ -45,16 +41,6 @@ namespace Saltyminer.Mining
             return Task.CompletedTask;
         }
 
-        // Task that is used to determine latest release for miners
-        public async Task<string> findLatest(string owner, string repository)
-        {
-            var client = new GitHubClient(new ProductHeaderValue("saltyminer-bot" + generateString()));
-            var releases = await client.Repository.Release.GetAll(owner, repository);
-            var latest = releases[0];
-            Debug.Write(latest.Name);
-            return latest.TagName;
-        }
-
         // Used to determine whether to display installed status or not
         public string checkInstalls(string folder)
         {
@@ -66,32 +52,6 @@ namespace Saltyminer.Mining
             else
             {
                 return "Not installed";
-            }
-        }
-
-        // Returns a random string for user agents
-        public static string generateString(int length = 16)
-        {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-            var random = new Random();
-            var randomString = new string(Enumerable.Repeat(chars, length)
-                                                    .Select(s => s[random.Next(s.Length)]).ToArray());
-            return randomString;
-        }
-
-        // Saves string array of installs into file
-        public void saveCurrentinstall()
-        {
-            File.WriteAllLines("currentVersions.txt", currentInstalls);
-        }
-
-        // Loads string array of installs into application
-        public void loadCurrentinstall()
-        {
-            using (StreamReader sr = new StreamReader("currentVersions.txt"))
-            {
-                currentInstalls = new[] { sr.ReadToEnd() };
             }
         }
     }
